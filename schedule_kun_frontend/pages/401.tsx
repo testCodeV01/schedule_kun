3c480aeb99c5ed34b1d772f3401319875239e350
TK1213\qft-t <qft-tk-1213@tk-labo.com>
2023-06-06 19:30:14 +0900

    いい感じのUIを作成するためのフレームワークを導入する #6 - materialUIの導入、他少し修正

--- schedule_kun_frontend/pages/401.tsx
+++ schedule_kun_frontend/pages/401.tsx
@@ -1,10 +1,14 @@
 import { NextPage } from 'next';
+import { useRouter } from 'next/router';
 
 const Error401: NextPage = () => {
+  const router = useRouter();
+
   return (
     <>
       <div>
         <span>401エラーです。</span>
+        <button onClick={() => router.push('login')}>移動</button>
       </div>
     </>
   );
