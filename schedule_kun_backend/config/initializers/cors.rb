747901c46f5b00523ccd4e82607b1f0be40f7c13
TK1213\qft-t <qft-tk-1213@tk-labo.com>
2023-07-09 02:15:29 +0900

    VercelとRenderの疎通のための設定

--- schedule_kun_backend/config/initializers/cors.rb
+++ schedule_kun_backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ["localhost:3010"]
+    origins ["localhost:3010", "https://schedule-kun-frontend.vercel.app/"]
 
     resource "*",
       headers: :any,
